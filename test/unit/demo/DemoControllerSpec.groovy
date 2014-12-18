a90707c2f26bf1ef9b885215f13cd22b746a4ed2
Jeff Scott Brown <jbrown@pivotal.io>
2014-12-18 12:32:47 -0600

    test invalid instance

--- test/unit/demo/DemoControllerSpec.groovy
+++ test/unit/demo/DemoControllerSpec.groovy
@@ -6,12 +6,21 @@ import spock.lang.Specification
 @TestFor(DemoController)
 class DemoControllerSpec extends Specification {
 
-    void "test something"() {
+    void "test valid command object"() {
         when:
         request.json = '{"causes":[{"name":"prabh","description":"ggg","icon":"deec"}]}'
         controller.saveCreateFunding()
 
         then:
         response.text == 'No errors were found'
+    }
+
+    void "test invalid command object"() {
+        when:
+        request.json = '{"causes":[{"ggg","icon":"deec"}]}'
+        controller.saveCreateFunding()
+
+        then:
+        response.text == 'Errors were found'
     }
 }
